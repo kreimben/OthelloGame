72d911cea0c154ee9cd52673a62af3ef4ca06407
Aksidion Kreimben <aksidion@kreimben.com>
2022-11-22 12:19:01 +0900

    클라이언트가 입장했을 때 보내는 request 객체.

---
+++ OthelloServer/Server/Request/EnterRequest.java
@@ -0,0 +1,33 @@
+package Server.Request;
+
+import Server.Person.Person;
+
+import java.util.Optional;
+
+public class EnterRequest extends GeneralRequest {
+    // 202 방에 입장 request. c -> s
+    private String userName;
+    private String roomName;
+
+    public EnterRequest(Person person, String roomName, String userName) {
+        super(202, person, Optional.empty());
+        this.userName = userName;
+        this.roomName = roomName;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getRoomName() {
+        return this.roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+}
